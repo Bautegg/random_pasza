50a3995712458f9a11d0d3bb65017f51417185f7
Bautegg <97198351+Bautegg@users.noreply.github.com>
2022-10-17 19:22:31 +0200

    Added description on top

--- random_pasza_main.py
+++ random_pasza_main.py
@@ -1,17 +1,9 @@
 """
+Program dedicated for people who don't know what to eat today!
 
-The pseudorandom number generator is a mathematical function that generates a sequence of nearly random numbers.
-
-It takes a parameter to start off the sequence, called the seed. The function is deterministic, meaning given the same seed,
-it will produce the same sequence of numbers every time. The choice of seed does not matter.
-
-The seed() function will seed the pseudorandom number generator, taking an integer value as an argument, such as 1 or 7.
-If the seed() function is not called prior to using randomness, the default is to use the current system time in milliseconds from epoch (1970).
-
-The example below demonstrates seeding the pseudorandom number generator,
-generates some random numbers, and shows that reseeding the generator will result in the same sequence of numbers being generated.
-
+***Bautegg***
 """
+
 
 from random import seed
 from random import randint
@@ -27,6 +19,3 @@ def random_value():
 
 random_value()
 
-"""
-podpiąć ceny z gazetek i przepisy, ma mi zwrócić najtańsze danie
-"""
