e7d4faca6b5e095040cba7abeb49d6e0f1f09046
Bautegg <97198351+Bautegg@users.noreply.github.com>
2022-10-17 20:25:07 +0200

    -added pandas -places are readed from .xlsx file -randint range dependent of number of rows in file

--- random_pasza_main.py
+++ random_pasza_main.py
@@ -5,17 +5,23 @@ Program dedicated for people who don't know what to eat today!
 """
 
 
-from random import seed
+#from random import seed
 from random import randint
+import pandas as pd
 def random_value():
-
+    data = pd.read_excel(r'C:\Users\User\Documents\GitHub\random_pasza\pasza_sheet.xlsx')
+    df = pd.DataFrame(data)
+    print(df)
+    print('ILE: ',len(df.place))
     #seed(0)
-    rand_value = randint(1, 5) # generate random number (from a, to b)
+    rand_value = randint(0, len(df.place)-1) # generate random number (from a, to b)
     print("Rand value: ", rand_value)
+    #
+    # places = dict({1: "Indian", 2: "China", 3: "Pizza", 4: "Burger", 5: "Sushi"})
+    #
+    # print(places[rand_value])
+    print(df.iloc[rand_value,0])
 
-    places = dict({1: "Indian", 2: "China", 3: "Pizza", 4: "Burger", 5: "Sushi"})
-
-    print(places[rand_value])
 
 random_value()
 
