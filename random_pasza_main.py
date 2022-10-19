6c4437b3335c8c87309c049fe5fea914327bd1eb
Bautegg <97198351+Bautegg@users.noreply.github.com>
2022-10-19 20:06:55 +0200

    -Added comments

--- random_pasza_main.py
+++ random_pasza_main.py
@@ -15,24 +15,24 @@ def random_value():
     print(option)
     if option == 'reset':
         path = r'C:\Users\User\Documents\GitHub\random_pasza\pasza_sheet.xlsx'
-        data = pd.read_excel(path)
+        data = pd.read_excel(path)  #load original file
         df = pd.DataFrame(data)
         df.to_excel('pasza_sheet_work.xlsx')
         print(df)
         random_value()
     elif option == 'rand':
         path = r'C:\Users\User\Documents\GitHub\random_pasza\pasza_sheet_work.xlsx'
-        data = pd.read_excel(path)
+        data = pd.read_excel(path)  #load working file
         df = pd.DataFrame(data)
         print(df)
         count_rows = len(df.place)
-        rand_value = randint(0, count_rows - 1)  # generate random number (from a, to b)
+        rand_value = randint(0, count_rows - 1)  # generate random number (from first, to last row)
         print("Rand value: ", rand_value)
         print(df.iloc[rand_value, 0])
         df = df.drop([rand_value])
         print(df)
-        df.to_excel('pasza_sheet_work.xlsx')
-    else:
+        df.to_excel('pasza_sheet_work.xlsx') #save file without drawed position
+    else:   #if user provide invalid input go to the begining of program
         random_value()
 
 random_value()
